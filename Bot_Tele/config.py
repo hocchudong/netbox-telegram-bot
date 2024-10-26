ddcffedd7d6603548a3fb59988466d06562f124a
Ducmanh28 <95476360+Ducmanh28@users.noreply.github.com>
2024-10-26 23:18:46 +0700

    netbox bot telegram

---
+++ Bot_Tele/config.py
@@ -0,0 +1,11 @@
+# Enter who can recieve the messenger from Bot
+ADMIN_IDS = ['Nhập user Telegram tại đây']
+
+# Enter the URL of NetBox
+URLNETBOX = "Nhập URL NetBox tại đây"              
+
+# Enter the NetBox Token                    
+TOKENNETBOX = "Nhập Token Netbox tại đây"
+
+# Enter the Token Bot Telegram
+TOKENTELEGRAM = "Nhập Token Bot tại đây"
